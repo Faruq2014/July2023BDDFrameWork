1eefa2dd95b9587c0b27117d1cff73bfb6ec72f3
Mohammed Molla <mjmolla@gmail.com>
2023-09-19 21:02:25 -0400

    runner file and base test

--- src/test/java/com/facebook/stepDefinations/LoginTest.java
+++ src/test/java/com/facebook/stepDefinations/LoginTest.java
@@ -1,24 +1,34 @@
 package com.facebook.stepDefinations;
 
+import com.facebook.base.BaseTest;
+import com.facebook.pages.LoginPage;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LoginTest {
+public class LoginTest extends BaseTest{
+	LoginPage loginPage;
 	
 	@Given("user open facebook home page")
 	public void user_open_facebook_home_page() {
-	 System.out.println("hello given");   
+	 System.out.println("hello given"); 
+	 openApp();
 	}
 
 	@When("user enter userName and password")
 	public void user_enter_user_name_and_password() {
-		System.out.println("hello when");   
+		System.out.println("hello when");
+		loginPage = new LoginPage(driver);
+		loginPage.userName("Ayon");
+		loginPage.enterPassword("aoyeerr");
 	}
 
 	@Then("user succesfully logged in")
 	public void user_succesfully_logged_in() {
-		System.out.println("hello then");    
+		System.out.println("hello then"); 
+		loginPage.clickLoginButton();
+		closeApp();
 	}
 
 }
