797b0f11b57df8f06082a80108be37db3bf52d13
Mohammed Molla <mjmolla@gmail.com>
2023-10-03 20:48:31 -0400

    screenShot

--- src/test/java/com/facebook/base/BaseTest.java
+++ src/test/java/com/facebook/base/BaseTest.java
@@ -1,14 +1,18 @@
 package com.facebook.base;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
-
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class BaseTest {
 
@@ -23,12 +27,53 @@ public class BaseTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
+
 	public void assertTitle(String actualTitle) {
-		String expectedTitle=driver.getTitle();
+		String expectedTitle = driver.getTitle();
 		Assert.assertEquals("Title din not match", expectedTitle, expectedTitle);
-		
+
 	}
+
+	public void screenShot(String screenShotName) {
+		// taking the screen shot of the current page
+		TakesScreenshot screenShot = ((TakesScreenshot) driver);
+		// source file name
+		File src = screenShot.getScreenshotAs(OutputType.FILE);
+		Long takingTiem = System.currentTimeMillis();
+		String destinationPath = "ScreenShots/" + screenShotName + takingTiem + ".png";
+		// above 2 lines combine, we created destination path for screen shot
+		// setting the destination path
+		File destinationFile = new File(destinationPath);
+
+		try {
+			// copy from the src file and paste in the destination file
+			FileUtils.copyFile(src, destinationFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void webElementScreenShot(WebElement element, String screenShotName) {
+		// WebElement userName=driver.findElement(By.id("email"));
+		File src = element.getScreenshotAs(OutputType.FILE);
+
+		Long takingTiem = System.currentTimeMillis();
+
+		String destinationPath = "ScreenShots/" + screenShotName + takingTiem + ".png";
+
+		File destinationFile = new File(destinationPath);
+
+		try {
+			FileUtils.copyFile(src, destinationFile);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
 	public void closeApp() {
 		driver.quit();
 	}
+
 }
